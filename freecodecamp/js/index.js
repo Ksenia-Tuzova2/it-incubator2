980e9b74b6a490294645414d375c2410b67aa0e4
Ksenia Tuzova <doll.garden@yandex.ru>
2022-07-22 00:22:14 +0300

    update js file

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -14,3 +14,13 @@ a = 7;
 var myFirstName='Ksenia'
 var myLastName = 'Tuzova'
 // Only change code below this line
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
