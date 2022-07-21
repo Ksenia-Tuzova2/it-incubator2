a9a5c05b10ae02f94200a447f1b7d18849b4356e
Ksenia Tuzova <doll.garden@yandex.ru>
2022-07-22 00:35:49 +0300

    update js file 2

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -24,3 +24,27 @@ var c = "I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+//camel case
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//use let 
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//use const 
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+const FACT = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//use multiplie
