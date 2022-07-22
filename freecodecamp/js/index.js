474e98ed6e3109e1037754bbd71368df57094e42
Ksenia Tuzova <doll.garden@yandex.ru>
2022-07-22 11:59:14 +0300

    update js file 5

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -104,3 +104,21 @@ let c = 4.6;
 a *= 5;
 b *= 3;
 c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//в конце первой строки должен быть пробел
+const myStr = "This is the start ." + "This is the end."; // Change this line
