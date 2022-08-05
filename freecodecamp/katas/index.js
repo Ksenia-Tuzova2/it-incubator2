42ef122ffe1295ff739376496dc855c18291287b
Ksenia Tuzova <doll.garden@yandex.ru>
2022-08-05 23:34:51 +0300

    new kata

---
+++ freecodecamp/katas/index.js
@@ -0,0 +1,15 @@
+//сделай любое число негативным
+function makeNegative(num) {
+	if (num < 0) return num
+	else if (num > 0) {
+		return -num
+	} else if (num == 0) return num
+}
+
+//переверни строку
+
+
+function solution(str) {
+	return str.split('').reverse().join('')
+}
+//check
